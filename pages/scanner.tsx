eb0fe4ade50d4c2c9487cf95e2db1717605123f6
allenlin90 <ht016746@gmail.com>
2022-07-05 08:49:15 +0700

    refactor: turn /scanner to getStaticProps

--- pages/scanner.tsx
+++ pages/scanner.tsx
@@ -1,12 +1,12 @@
 import type { ReactNode } from 'react';
-import type { GetServerSideProps } from 'next';
+import type { GetStaticProps } from 'next';
 import type { NextPageWithLayout } from './_app';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import Seo from 'components/common/Seo';
 import Layout from 'components/common/Layout/Layout';
 import ScannerPanel from 'components/scanner/ScannerPanel';
 
-export const getServerSideProps: GetServerSideProps = async ({ locale }) => {
+export const getStaticProps: GetStaticProps = async ({ locale }) => {
   return {
     props: {
       ...(locale &&
