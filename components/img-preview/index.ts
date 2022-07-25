c67c3a945ce422e0c1f1264b2fb9f193462e2fc7
allenlin90 <ht016746@gmail.com>
2022-07-25 23:44:28 +0700

    feat: add image upload and preview

--- components/img-preview/index.ts
+++ components/img-preview/index.ts
@@ -1,3 +1,6 @@
+export * from './AddImage';
 export * from './ImgPreview';
 export * from './ImageThumb';
+export * from './ImgUpload';
 export * from './Placeholder';
+export * from './UploadProgress';
